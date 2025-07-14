20d29cb794052e45f35241bd186de6a259f58022
Berk Polat <berkpolat125@gmail.com>
2025-07-14 18:39:38 +0200

    branch + tenant entities added

---
+++ AppointmentSystem.Domain/Entities/Branch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Domain.Entities
+{
+    public class Branch
+    {
+        public Guid BranchId { get; set; }
+        public Guid TenantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Tenant Tenant { get; set; }
+    }
+
+}
---
+++ AppointmentSystem.Domain/Entities/Tenant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Domain.Entities
+{
+    public class Tenant
+    {
+        public Guid TenantId { get; set; }
+        public string Name { get; set; }
+        public string Domain { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Plan { get; set; }
+        public string Status { get; set; }
+
+        public ICollection<Branch> Branches { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+
+}
