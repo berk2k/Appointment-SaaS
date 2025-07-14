bbc685de5eaf7ceca6b5327a35eb4cdce0a3d747
Berk Polat <berkpolat125@gmail.com>
2025-07-14 19:05:23 +0200

    db infra added

---
+++ AppointmentSystem.Infrastructure/Services/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppointmentSystem.Infrastructure.Services
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("AppointmentProjectDefaultString")));
+
+            return services;
+        }
+    }
+
+}
