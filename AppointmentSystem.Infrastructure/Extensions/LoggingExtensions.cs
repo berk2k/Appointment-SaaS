3b756634dc892636cd18da0cff49b78c8c87cc94
Berk Polat <berkpolat125@gmail.com>
2025-07-14 20:38:36 +0200

    serilog logging extension added

---
+++ AppointmentSystem.Infrastructure/Extensions/LoggingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace AppointmentSystem.Infrastructure.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static IHostBuilder UseCustomSerilog(this IHostBuilder hostBuilder, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            return hostBuilder.UseSerilog();
+        }
+    }
+}
