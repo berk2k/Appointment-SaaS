8e14e0382a4c0896c44ed5e204ed18c2807d54d8
Berk Polat <berkpolat125@gmail.com>
2025-07-17 17:42:07 +0200

    command added

--- AppointmentSystem.Application/Commands/Appointment/CreateAppointmentCommand.cs
+++ AppointmentSystem.Application/Commands/Appointment/CreateAppointmentCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppointmentSystem.Domain.Enums;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace AppointmentSystem.Application.Commands.Appointment
 {
-    internal class CreateAppointmentCommand
+    public class CreateAppointmentCommand : IRequest<Guid>
     {
+        public Guid TenantId { get; set; }
+        public Guid BranchId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CustomerId { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public AppointmentStatus Status { get; set; }
     }
 }
