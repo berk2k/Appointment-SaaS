126403429171d6887aa80fe1c8b014d37134c4f7
Berk Polat <berkpolat125@gmail.com>
2025-07-24 15:09:50 +0200

    customer repo

---
+++ AppointmentSystem.Domain/Interfaces/Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Domain.Interfaces.Repositories;
+
+namespace AppointmentSystem.Domain.Interfaces
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        Task<Customer?> GetByEmailAsync(string email);
+        Task<Customer?> GetByPhoneNumberAsync(string email);
+        Task<Customer?> GetWithAppointmentsAsync(Guid customerId);
+    }
+}
---
+++ AppointmentSystem.Infrastructure/Repositories/ICustomerRepository.cs
@@ -0,0 +1,61 @@
+﻿using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Domain.Interfaces;
+using AppointmentSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystem.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CustomerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Customer?> GetByIdAsync(Guid id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task AddAsync(Customer entity)
+        {
+            await _context.Customers.AddAsync(entity);
+        }
+
+        public void Update(Customer entity)
+        {
+            _context.Customers.Update(entity);
+        }
+
+        public void Delete(Customer entity)
+        {
+            _context.Customers.Remove(entity);
+        }
+
+        public async Task<Customer?> GetByEmailAsync(string email)
+        {
+            return await _context.Customers
+                .FirstOrDefaultAsync(c => c.Email.Value == email);
+        }
+
+        public async Task<Customer?> GetByPhoneNumberAsync(string phoneNumber)
+        {
+            return await _context.Customers
+                .FirstOrDefaultAsync(c => c.PhoneNumber.Value == phoneNumber);
+        }
+
+        public async Task<Customer?> GetWithAppointmentsAsync(Guid customerId)
+        {
+            return await _context.Customers
+                .Include(c => c.Appointments)
+                .FirstOrDefaultAsync(c => c.CustomerId == customerId);
+        }
+    }
+}
