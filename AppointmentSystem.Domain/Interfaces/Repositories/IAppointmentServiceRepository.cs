e7160c7b78a2cb9cf3b06c507e48e87747a4e5bb
Berk Polat <berkpolat125@gmail.com>
2025-07-24 15:47:14 +0200

    appointment-service relationship repo

---
+++ AppointmentSystem.Domain/Interfaces/Repositories/IAppointmentServiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using AppointmentSystem.Domain.Entities;
+
+namespace AppointmentSystem.Domain.Interfaces
+{
+    public interface IAppointmentServiceRepository
+    {
+        Task AddAsync(AppointmentService entity);
+        Task RemoveAsync(Guid appointmentId, Guid serviceId);
+        Task<IEnumerable<AppointmentService>> GetByAppointmentIdAsync(Guid appointmentId);
+        Task<IEnumerable<AppointmentService>> GetByServiceIdAsync(Guid serviceId);
+        Task<AppointmentService?> GetAsync(Guid appointmentId, Guid serviceId);
+    }
+}
---
+++ AppointmentSystem.Infrastructure/Repositories/AppointmentServiceRepository.cs
@@ -0,0 +1,59 @@
+﻿using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Domain.Interfaces;
+using AppointmentSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystem.Infrastructure.Repositories
+{
+    public class AppointmentServiceRepository : IAppointmentServiceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentServiceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(AppointmentService entity)
+        {
+            await _context.AppointmentServices.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Guid appointmentId, Guid serviceId)
+        {
+            var entity = await _context.AppointmentServices
+                .FirstOrDefaultAsync(a => a.AppointmentId == appointmentId && a.ServiceId == serviceId);
+
+            if (entity != null)
+            {
+                _context.AppointmentServices.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<AppointmentService>> GetByAppointmentIdAsync(Guid appointmentId)
+        {
+            return await _context.AppointmentServices
+                .Include(a => a.Service)
+                .Where(a => a.AppointmentId == appointmentId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<AppointmentService>> GetByServiceIdAsync(Guid serviceId)
+        {
+            return await _context.AppointmentServices
+                .Include(a => a.Appointment)
+                .Where(a => a.ServiceId == serviceId)
+                .ToListAsync();
+        }
+
+        public async Task<AppointmentService?> GetAsync(Guid appointmentId, Guid serviceId)
+        {
+            return await _context.AppointmentServices
+                .Include(a => a.Appointment)
+                .Include(a => a.Service)
+                .FirstOrDefaultAsync(a => a.AppointmentId == appointmentId && a.ServiceId == serviceId);
+        }
+    }
+}
