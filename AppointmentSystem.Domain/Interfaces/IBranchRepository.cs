d4591f483e8353b371317c77aa523b28abc0c54f
Berk Polat <berkpolat125@gmail.com>
2025-07-24 14:31:34 +0200

    branch repo added

---
+++ AppointmentSystem.Domain/Interfaces/IBranchRepository.cs
@@ -0,0 +1,10 @@
+﻿using AppointmentSystem.Domain.Entities;
+
+namespace AppointmentSystem.Domain.Interfaces
+{
+    public interface IBranchRepository : IRepository<Branch>
+    {
+        Task<IEnumerable<Branch>> GetByTenantIdAsync(Guid tenantId);
+        Task<Branch?> GetWithUsersAsync(Guid branchId);
+    }
+}
---
+++ AppointmentSystem.Infrastructure/Repositories/BranchRepository.cs
@@ -0,0 +1,58 @@
+﻿using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Domain.Interfaces;
+using AppointmentSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystem.Infrastructure.Repositories
+{
+    public class BranchRepository : IBranchRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BranchRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Branch?> GetByIdAsync(Guid id)
+        {
+            return await _context.Branches.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Branch>> GetAllAsync()
+        {
+            return await _context.Branches.ToListAsync();
+        }
+
+        public async Task AddAsync(Branch entity)
+        {
+            await _context.Branches.AddAsync(entity);
+        }
+
+        public void Update(Branch entity)
+        {
+            _context.Branches.Update(entity);
+        }
+
+        public void Delete(Branch entity)
+        {
+            _context.Branches.Remove(entity);
+        }
+
+        public async Task<IEnumerable<Branch>> GetByTenantIdAsync(Guid tenantId)
+        {
+            return await _context.Branches
+                .Where(b => b.TenantId == tenantId)
+                .ToListAsync();
+        }
+
+        public async Task<Branch?> GetWithUsersAsync(Guid branchId)
+        {
+            return await _context.Branches
+                .Include(b => b.UserBranches)      // Branch-User pivot table
+                    .ThenInclude(ub => ub.User)    
+                .FirstOrDefaultAsync(b => b.BranchId == branchId);
+        }
+
+    }
+}
