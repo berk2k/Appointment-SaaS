06059c72d6c8fd9bdc96bf694c3cd78df47e3308
Berk Polat <berkpolat125@gmail.com>
2025-07-24 15:48:40 +0200

    appointment history added

---
+++ AppointmentSystem.Domain/Interfaces/Repositories/IAppointmentHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using AppointmentSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Domain.Interfaces
+{
+    public interface IAppointmentHistoryRepository
+    {
+        Task AddAsync(AppointmentHistory entity);
+        Task<IEnumerable<AppointmentHistory>> GetByAppointmentIdAsync(Guid appointmentId);
+        Task<IEnumerable<AppointmentHistory>> GetByUserIdAsync(Guid userId);
+        Task<AppointmentHistory?> GetByIdAsync(Guid appointmentHistoryId);
+    }
+}
---
+++ AppointmentSystem.Infrastructure/Repositories/AppointmentHistoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using AppointmentSystem.Domain.Entities;
+using AppointmentSystem.Domain.Interfaces;
+using AppointmentSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystem.Infrastructure.Repositories
+{
+    public class AppointmentHistoryRepository : IAppointmentHistoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentHistoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(AppointmentHistory entity)
+        {
+            await _context.AppointmentHistories.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<AppointmentHistory>> GetByAppointmentIdAsync(Guid appointmentId)
+        {
+            return await _context.AppointmentHistories
+                .Include(h => h.ChangedByUser)
+                .Where(h => h.AppointmentId == appointmentId)
+                .OrderByDescending(h => h.ChangedAt)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<AppointmentHistory>> GetByUserIdAsync(Guid userId)
+        {
+            return await _context.AppointmentHistories
+                .Include(h => h.Appointment)
+                .Where(h => h.ChangedByUserId == userId)
+                .OrderByDescending(h => h.ChangedAt)
+                .ToListAsync();
+        }
+
+        public async Task<AppointmentHistory?> GetByIdAsync(Guid appointmentHistoryId)
+        {
+            return await _context.AppointmentHistories
+                .Include(h => h.ChangedByUser)
+                .Include(h => h.Appointment)
+                .FirstOrDefaultAsync(h => h.AppointmentHistoryId == appointmentHistoryId);
+        }
+    }
+}
