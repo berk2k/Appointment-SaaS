30b5905996cbf5f9c78a280454c6d8991a72850b
Berk Polat <berkpolat125@gmail.com>
2025-07-25 16:59:36 +0200

    403 custom exception

---
+++ AppointmentSystem.API/Handlers/ForbiddenExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentSystem.API.Handlers.Interfaces;
+using AppointmentSystem.Application.Exceptions;
+using System.Text.Json;
+
+namespace AppointmentSystem.API.Handlers
+{
+    public class ForbiddenExceptionHandler : IExceptionHandler
+    {
+        public bool CanHandle(Exception exception) => exception is CustomForbiddenException;
+
+        public async Task HandleAsync(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = 403;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                Message = "Forbidden",
+                Details = exception.Message
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+       
+    }
+}
---
+++ AppointmentSystem.Application/Exceptions/CustomForbiddenException.cs
@@ -0,0 +1,8 @@
+﻿namespace AppointmentSystem.Application.Exceptions
+{
+    internal class CustomForbiddenException : Exception
+    {
+        public CustomForbiddenException(string message) : base(message) { }
+        public CustomForbiddenException() : base("Forbidden access") { }
+    }
+}
