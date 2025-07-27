ae3f1adffcf6b33625cff9ef337976cf7385284b
Berk Polat <berkpolat125@gmail.com>
2025-07-27 19:07:13 +0300

    branch dtos added

--- AppointmentSystem.Application/DTOs/Branch/BranchResponse.cs
+++ AppointmentSystem.Application/DTOs/Branch/BranchResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppointmentSystem.Application.DTOs.Branch
+﻿namespace AppointmentSystem.Application.DTOs.Branch
 {
     public class BranchResponse
     {
@@ -12,5 +6,7 @@ namespace AppointmentSystem.Application.DTOs.Branch
         public string Name { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
---
+++ AppointmentSystem.Application/DTOs/Branch/BranchRoleResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AppointmentSystem.Application.DTOs.Branch
+{
+    public class BranchRoleResponse
+    {
+        public Guid BranchId { get; set; }
+        public string BranchName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
